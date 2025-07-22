51c89fb477ad6e72772cefd9aed612074a7ba083
Damir Tagankhozhaev <tagankhozhaev@gmail.com>
2025-07-22 19:33:18 +0500

    test2

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -28,7 +28,7 @@ const LoginPage: FC = () => {
             <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow-lg">
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Welcome asd
+                        asdasdasd
                     </h2>
                 </div>
                 <a
